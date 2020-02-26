c03ba8624a3a04b161f1b4d15a2e5f6ae9c83b60
shazzzam <kovalenkoor@gmail.com>
2020-02-26 18:23:53 +0600

    add main.scss, MainLayout.vue, base routing model

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  .inner-page
+    nav
+      ul.main-nav
+        li.main-nav__item
+          router-link(:to="{name: 'home'}") Home
+        li.main-nav__item
+          router-link(:to="{name: 'about'}") About
+    sidebar
+    router-view
+</template>
+
+<script>
+import '@/assets/scss/main.scss'
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -6,6 +6,6 @@
 
 <style lang="scss">
 h1 {
-  color: $bg;
+  color: $bg-dark;
 }
 </style>
